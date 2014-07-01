30dbc17970d6d242c133252e5a8e0b3d7db6141e
Nathan Jervis <mirhagk@gmail.com>
2014-07-01 00:19:42 -0400

    Initial visual studio project

---
+++ StaticAPI/StaticAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace StaticAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.LoadFile(args[0]);
+            foreach (var type in assembly.DefinedTypes.Where(t => t.Name.EndsWith("Controller")))
+                Console.WriteLine("{0} {1}", type.Name, type);
+        }
+    }
+}
